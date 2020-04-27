798e1511acc7749ccf5c13f9586e08e5e88e39c3
vss-s <53346463+vss-s@users.noreply.github.com>
2020-04-27 11:04:07 +0300

    Updates

---
+++ precache-manifest.8ed0b4a87fc98212003377771c9fb363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c50ea40b0069e130eb1b68451675cc7",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "235476caae1d9e951127",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.121b0054.chunk.css"
+  },
+  {
+    "revision": "fdd00aaad8365514efa9",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c8f49b24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.c8f49b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235476caae1d9e951127",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.befc943f.chunk.js"
+  },
+  {
+    "revision": "494cb0e9145d47a41a1a",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.c77db088.js"
+  }
+]);
